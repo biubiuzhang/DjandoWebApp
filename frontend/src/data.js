7a5fe66c94975adffe794137e7cc3d7ec20bc0f7
Nancy Zhang <nancy.zhang@mmbresearch.com>
2022-02-13 09:37:06 -0500

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,38 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "T-shirt",
+      category: "Shirts",
+      image: "/images/product-1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      numReview: 10,
+      description: "high quatlity"
+    },
+    {
+      _id: "2",
+      name: "T-shirt",
+      category: "Shirts",
+      image: "/images/product-2.jpg",
+      price: 110,
+      brand: "Nike",
+      rating: 4.5,
+      numReview: 10,
+      description: "high quatlity"
+    },
+    {
+      _id: "3",
+      name: "T-shirt",
+      category: "Shirts",
+      image: "/images/product-3.jpg",
+      price: 150,
+      brand: "Nike",
+      rating: 4.5,
+      numReview: 10,
+      description: "high quatlity"
+    }
+  ]
+};
+export default data;
