5cf801c335f046fcf47efc52ea073b318d5e9a6a
Nancy Zhang <zhangruonanee@gmail.com>
2022-10-16 10:41:28 -0400

    Return article title for admin page, or it will return Article(object)

--- MyWebsite/API/models.py
+++ MyWebsite/API/models.py
@@ -7,3 +7,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
