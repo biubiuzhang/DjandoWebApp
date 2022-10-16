3160157bc48935a7bd311b975334f80d72271fe1
Nancy Zhang <zhangruonanee@gmail.com>
2022-10-16 11:28:09 -0400

    Add serializer for Article model

--- MyWebsite/API/serializers.py
+++ MyWebsite/API/serializers.py
@@ -1,15 +1,19 @@
 from rest_framework import serializers
 from .models import Article
 
+# To user serializer
+#   1. python manage.py shell
+#   2. from API.serializers import ArticleSerializer
+#   3. s = ArticleSerializer()
+#   4. print(repr(s))
+#   5. Output:
+#      ArticleSerializer():
+#      id = IntegerField(label='ID', read_only=True)
+#      title = CharField(max_length=100)
+#      description = CharField(style={'base_template': 'textarea.html'})
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=500)
 
-    def create(self, validated_data):
-        return Article.objects.create(validated_data)
-
-    def udpate(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get(
-            "description", instance.description)
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'description']
