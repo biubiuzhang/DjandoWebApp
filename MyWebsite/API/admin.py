de5350a3c6f3b32ae2146f935f476b0db162f2c6
Nancy Zhang <zhangruonanee@gmail.com>
2022-10-16 10:40:38 -0400

    Add filter and column title to Article admin page

--- MyWebsite/API/admin.py
+++ MyWebsite/API/admin.py
@@ -3,4 +3,10 @@ from .models import Article
 
 # Register your models here.
 
-admin.site.register(Article)
+# admin.site.register(Article)
+
+
+@admin.register(Article)
+class ArticleModel(admin.ModelAdmin):
+    list_filter = ("title", "description")
+    list_display = ("title", "description")
